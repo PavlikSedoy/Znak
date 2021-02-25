2cc00f463596d8cf81c76f6014eb91c2b9420a6e
pavliksedoy <pavliksedoy@gmail.com>
2021-02-25 09:16:55 +0200

    Btn df --dark

---
+++ src/assets/js/components/portfolio.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper/bundle'
+
+const portfolioSlider = new Swiper('#portfolioSlider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+})
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,4 @@
 import '@styles/index.scss';
 
 import '@js/utils/general'
+import '@js/components/portfolio'
