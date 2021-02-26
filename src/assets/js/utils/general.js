8a643b5397dd3e09a7a862a05ccdcaf7b37317ee
pavliksedoy <pavliksedoy@gmail.com>
2021-02-26 12:30:30 +0200

    Show more button (clients section)

--- src/assets/js/utils/general.js
+++ src/assets/js/utils/general.js
@@ -2,5 +2,5 @@ import fullpage from "fullpage.js/dist/fullpage"
 
 new fullpage('#fullpage', {
     licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
-    anchors:['home', 'acquaintance', 'portfolio', 'why-us', 'your-object', 'vizualization', 'design-project'],
+    anchors:['home', 'acquaintance', 'portfolio', 'why-us', 'your-object', 'vizualization', 'design-project', 'clients-and-reviews'],
 })
