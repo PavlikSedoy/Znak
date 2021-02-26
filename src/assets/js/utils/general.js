2e05cc5672e629f03849368fcac84f6d45ea1382
pavliksedoy <pavliksedoy@gmail.com>
2021-02-26 19:38:15 +0200

    Scroll status functionality

--- src/assets/js/utils/general.js
+++ src/assets/js/utils/general.js
@@ -1,6 +1,12 @@
 import fullpage from "fullpage.js/dist/fullpage"
 
-const header = document.getElementById('header')
+const header = document.getElementById('header'),
+    totalSections = document.getElementsByClassName('section').length
+const currentScroll = document.getElementById('currentScroll'),
+    totalScrolls = document.getElementById('totalScrolls'),
+    scrollVizual = document.getElementById('scrollVizual')
+
+const scrollHeight = scrollVizual.parentElement.offsetHeight  / totalSections
 
 new fullpage('#fullpage', {
     licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
@@ -11,5 +17,18 @@ new fullpage('#fullpage', {
         } else if ( destination.index == 0 ) {
             header.classList.remove('header--active')
         }
+
+        const currentIndex = destination.index < 9 ? '0' + (destination.index + 1) : destination.index + 1
+
+        currentScroll.innerHTML = currentIndex
+        scrollVizual.style.top =  scrollHeight * destination.index + 'px'
+
+    },
+    afterRender: () => {
+        totalScrolls.innerHTML = totalSections < 10 ? '0' + totalSections : totalSections
+
+        scrollVizual.style.height = scrollHeight + 'px'
     }
 })
+
+// Scroll height
