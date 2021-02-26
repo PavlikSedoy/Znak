d825afc3c0b1c8c716c4b148ec6c75bf46f5e2dc
pavliksedoy <pavliksedoy@gmail.com>
2021-02-26 19:05:33 +0200

    Changing heade ron scroll

--- src/assets/js/utils/general.js
+++ src/assets/js/utils/general.js
@@ -1,6 +1,15 @@
 import fullpage from "fullpage.js/dist/fullpage"
 
+const header = document.getElementById('header')
+
 new fullpage('#fullpage', {
     licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
     anchors:['home', 'acquaintance', 'portfolio', 'why-us', 'your-object', 'vizualization', 'design-project', 'clients-and-reviews', 'request-form', 'footer'],
+    onLeave: (origin, destination, direction) => {
+        if ( origin.index == 0 ) {
+            header.classList.add('header--active')
+        } else if ( destination.index == 0 ) {
+            header.classList.remove('header--active')
+        }
+    }
 })
