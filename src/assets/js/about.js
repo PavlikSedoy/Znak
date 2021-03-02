62a6e26e1673e9e228bf8b613ef3c4cdcd63441d
pavliksedoy <pavliksedoy@gmail.com>
2021-03-02 04:34:57 +0200

    Full page - add scroll into sections

--- src/assets/js/about.js
+++ src/assets/js/about.js
@@ -5,12 +5,14 @@ import '@js/components/portfolio'
 import '@js/components/mission-slider'
 
 import fullpage from "fullpage.js/dist/fullpage"
+import "fullpage.js/vendors/scrolloverflow"
 import ScrollStatus from "@js/classes/ScrollStatus"
 import Header from "@js/classes/Header"
 
 new fullpage('#fullpage', {
     licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
     anchors:['home', 'mission', 'portfolio', 'request-form', 'footer'],
+    scrollOverflow: true,
     onLeave: (origin, destination, direction) => {
         Header.changeHeaderTheme( origin, destination )
         ScrollStatus.sectionChanged( destination )
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,12 +6,14 @@ import '@js/components/reviews'
 import '@js/components/request-form'
 
 import fullpage from "fullpage.js/dist/fullpage"
+import "fullpage.js/vendors/scrolloverflow"
 import ScrollStatus from "@js/classes/ScrollStatus"
 import Header from "@js/classes/Header"
 
 new fullpage('#fullpage', {
     licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
     anchors:['home', 'acquaintance', 'portfolio', 'why-us', 'your-object', 'vizualization', 'design-project', 'clients-and-reviews', 'request-form', 'footer'],
+    scrollOverflow: true,
     onLeave: (origin, destination, direction) => {
         Header.changeHeaderTheme( origin, destination )
         ScrollStatus.sectionChanged( destination )
