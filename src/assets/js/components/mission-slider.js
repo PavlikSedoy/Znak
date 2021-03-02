a21c9bfe6ef7723f34391b7f02148660ff54b05e
pavliksedoy <pavliksedoy@gmail.com>
2021-03-02 04:10:12 +0200

    Image pagination. About page

--- src/assets/js/components/mission-slider.js
+++ src/assets/js/components/mission-slider.js
@@ -8,4 +8,24 @@ const missionSlider = new Swiper('#missionSlider', {
         prevEl: '#missionSliderPrev',
         nextEl: '#missionSliderPrev',
     },
+    pagination: {
+        el: '#missionSliderNavigation',
+        type: 'custom',
+        renderCustom: ( swiper, current, total ) => {   
+            const showedCurrent = current < 10 ? '0' + current : current
+            const showedTotal = total < 10 ? '0' + total : total
+
+            const progressPrecentage = 1 / total * 100
+            const progressLeft = 100 * current - 100
+
+            const currentEl = '<div class="img-pagination__number img-pagination__number--current">' + showedCurrent + '</div>'
+            const totalEl = '<div class="img-pagination__number img-pagination__number--total">' + showedTotal + '</div>'
+
+            const progress = '<div class="img-pagination__progress-block"><div class="img-pagination__proggress-line" style="width: ' + progressPrecentage + '%; transform: translateX(' + progressLeft + '%)"></div></div>'
+
+            const html = currentEl + progress + totalEl
+            
+            return html
+        }
+    },
 })
