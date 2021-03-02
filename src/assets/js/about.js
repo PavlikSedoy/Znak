99a8adb5b77eca2db86f947353517fc5817230e3
pavliksedoy <pavliksedoy@gmail.com>
2021-03-02 04:19:50 +0200

    Full page to current pages

--- src/assets/js/about.js
+++ src/assets/js/about.js
@@ -3,3 +3,19 @@ import '@styles/about.scss'
 import '@js/utils/general'
 import '@js/components/portfolio'
 import '@js/components/mission-slider'
+
+import fullpage from "fullpage.js/dist/fullpage"
+import ScrollStatus from "@js/classes/ScrollStatus"
+import Header from "@js/classes/Header"
+
+new fullpage('#fullpage', {
+    licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
+    anchors:['home', 'mission', 'portfolio', 'request-form', 'footer'],
+    onLeave: (origin, destination, direction) => {
+        Header.changeHeaderTheme( origin, destination )
+        ScrollStatus.sectionChanged( destination )
+    },
+    afterRender: () => {
+        ScrollStatus.firstLoad()
+    }
+})
--- src/assets/js/utils/general.js
+++ src/assets/js/utils/general.js
@@ -1,19 +1,3 @@
-import fullpage from "fullpage.js/dist/fullpage"
-import ScrollStatus from "@js/classes/ScrollStatus"
-import Header from "@js/classes/Header"
-
-new fullpage('#fullpage', {
-    licenseKey: '643A8C2B-18D047C0-845E464B-28BFE23E',
-    anchors:['home', 'acquaintance', 'portfolio', 'why-us', 'your-object', 'vizualization', 'design-project', 'clients-and-reviews', 'request-form', 'footer'],
-    onLeave: (origin, destination, direction) => {
-        Header.changeHeaderTheme( origin, destination )
-        ScrollStatus.sectionChanged( destination )
-    },
-    afterRender: () => {
-        ScrollStatus.firstLoad()
-    }
-})
-
 // Language
 const langBtn = document.getElementById('lngBtn'),
     langList = document.getElementById('lngList')
